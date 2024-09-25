1584f6dcc065cc05341540818f8f6d145e202967
Andrei Gatti <andreigatti1990@gmail.com>
2024-09-25 11:21:04 -0300

    Criação da classe Produto e utilização de ArrayList com Construtor

--- src/br/com/challenges/curso3/Main.java
+++ src/br/com/challenges/curso3/Main.java
@@ -1,12 +1,13 @@
 package br.com.challenges.curso3;
 
 import br.com.challenges.curso3.model.Person;
+import br.com.challenges.curso3.model.Product;
 
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Person> personArrayList = new ArrayList<>();
+        /*ArrayList<Person> personArrayList = new ArrayList<>();
 
         Person person1 = new Person("Akemi", 18);
         Person person2 = new Person("Rodrigo", 30);
@@ -22,7 +23,19 @@ public class Main {
         System.out.println("Lista de Pessoas:");
         for (Person person : personArrayList){
             System.out.println(person);
-        }
+        }*/
+
+        ArrayList<Product> productsList = new ArrayList<>();
+
+        Product product1 = new Product("Sabão", 15.99 , 5);
+        Product product2 = new Product("Detergente", 10.49, 8);
+
+        productsList.add(product1);
+        productsList.add(product2);
+
+        System.out.println("Tamanho da lista: " + productsList.size());
+        System.out.println("Produto na posição 0: " + productsList.get(0).getName());
+
     }
 
 
